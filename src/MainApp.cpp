3004d4e802230f0b09ec93b8bcecb1e1d6b34a7a
horia.goran <horia.goran@eu.tokeninc.com>
2024-09-06 10:05:13 +0300

    improvement

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -75,7 +75,7 @@ int __cdecl main(int argc, char** argv) {
 
 			std::cin >> option;
 
-			if (option < 1 || option > 9) {
+			if (option < 1 || option > 23) {
 				std::cin.clear();
 				std::cout << "Wrong choice." << std::endl;
 				return 0;
